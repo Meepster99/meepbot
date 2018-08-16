77d19a2e5305c8b98696e5297c59a9d866844747
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2018-08-16 14:04:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ function respond() {
   }
   
   //below this is stuff u added
-  botRegex = /trumpets/;
+  botRegex = /trumpets/i;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
