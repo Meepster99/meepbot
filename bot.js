c3aac15ff363168e259eb760abdb75db897d07ca
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2018-08-16 13:58:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,56 @@ function respond() {
     this.res.writeHead(200);
     this.res.end();
   }
+  
+  //below this is stuff u added
+  botRegex = /^\trumpets/;
+
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postString("suck");
+    this.res.end();
+  }
 }
+
+
+
+function postString(str) {
+  var botResponse, options, body, botReq;
+
+  botResponse = str;
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+
 
 function postMessage() {
   var botResponse, options, body, botReq;
