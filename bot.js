e8c05c2b2de4637615fa42ecd841f5329eb09a27
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2018-08-16 20:47:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,10 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/test/;
 
+  if( request.user_id == "62042029" ) {
+    this.res.end();
+  }
+  
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
